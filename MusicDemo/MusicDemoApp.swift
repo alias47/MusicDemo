ef7b1b73cde6a748517afcb624e74ae8cea09450
Atit Raj Kayastha <arkayastha@devfinity.com>
2026-03-14 16:24:23 +0545

    Initial Commit

---
+++ MusicDemo/MusicDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicDemoApp.swift
+//  MusicDemo
+//
+//  Created by Devfinity_LLC on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MusicDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
